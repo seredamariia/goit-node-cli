abc3d88dd0b9293de12c5172ad39dacd1716fc33
Mariia Sereda <seredamariiav@gmail.com>
2024-06-11 02:37:26 +0300

    Performed Step 3 of homework.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { program } from "commander";
+const { program } = require("commander");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,23 +17,26 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const contacts = await listContacts();
+      console.table(contacts);
       break;
 
     case "get":
-      // ... id
+      const contact = await getContactById(id);
+      console.table(contact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await addContact({ name, email, phone });
+      console.table(newContact);
       break;
 
     case "remove":
-      // ... id
+      const removedContact = await removeContact(id);
+      console.table(removedContact);
       break;
 
     default:
